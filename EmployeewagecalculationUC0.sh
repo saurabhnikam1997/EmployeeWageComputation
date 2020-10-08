fd7da1197e42ad12393aeb20e936d3ea2540e237
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-08 21:57:53 +0530

    Employee_Attendence_Check1

--- EmployeewagecalculationUC0.sh
+++ EmployeewagecalculationUC0.sh
@@ -1,3 +1,18 @@
 #!/bin/bash -x
 
 echo "WELCOME To Employee Wage Calculation"
+
+
+isPresent=1
+
+function attendence()
+{
+	random=$((RANDOM % 2 ))
+	if [[ $random -eq $isPresent ]]
+	then
+		echo "Employee is Present"
+	else
+		echo "Employee is Absent"
+	fi
+}
+attendence  
